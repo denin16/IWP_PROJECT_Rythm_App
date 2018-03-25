0a751c2811ba1405114c68793e1c7c8de12fa552
denin16 <navdeep.beniwal1997@gmail.com>
2018-03-25 14:03:49 +0530

    initial commit

--- main.php
+++ main.php
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-<?php
-    //Connecting the database
-    require "connect_db.php";
-
-    //Starting the session for user
-    session_start();
-
-=======
 <?php 
     //connecting to database
     require "connect_db.php";
 
     //Starting the user session
     session_start();
->>>>>>> branch1
     //Getting the user id
     $user_id = $_SESSION["user_id"];
 
@@ -22,11 +12,7 @@
         header("Location: connection_error.php");
     }
 
-<<<<<<< HEAD
-?>
-
-=======
-    /*-------------------------------------------------------------
+   /*-------------------------------------------------------------
     Script to display all the new songs present in the database
     --------------------------------------------------------------*/
     $limit = "";
@@ -62,10 +48,9 @@
         //Error in executing the query
         echo "Error executing the query";
     }
-        
-    
+
 ?>
->>>>>>> branch1
+
 
 <!DOCTYPE html>
 <html>
@@ -88,30 +73,15 @@
     }
     p {
         color: #ddd;
-<<<<<<< HEAD
-=======
         font-size: 15px;
->>>>>>> branch1
     }
     h3 {
         color: #ddd;
     }
-<<<<<<< HEAD
-    .card {
-        padding: 0px;
-        color: #ddd;
-        background: #111;
-        border: 1px solid #222222;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.25), 0 6px 20px 0 rgba(0, 0, 0, 0.25); 
-    }
-    
-=======
-
     .music-card {
         margin: 8px;
         margin-bottom: 15px;
     }
->>>>>>> branch1
     .card-body {
         margin: 0px;
         padding: 0px;
@@ -121,347 +91,14 @@
         padding-left: 5px;
         color: #ddd;
     }
-<<<<<<< HEAD
-    .card:hover  .card-img-top {
-=======
     .music-card:hover  .card-img-top {
->>>>>>> branch1
         opacity: 0.4;
     }
 </style>
 </head>
 <body>
 <div class="container">
-    
-<<<<<<< HEAD
-    <!-- Recently Played -->
-    <h3>Recently Played ...</h3>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/israel-palacio-459693.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-    <br>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/nkululeko-mabena-436777.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/austin-neill-222825.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="center">
-                <img class="card-img-top img-fluid img-responsive" src="images/ben-neale-194644.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/simon-maennling-27234.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/singer-540771__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-
-    </div>
-    <div class="text-right" style="padding: 3px;">
-        <p><a href="#" class="text-muted">See all...</a></p>
-    </div>
-    <br>
-    <h3 id="newly_added">Newly added...</h3>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/israel-palacio-459693.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/4902320203_29e3235af5_z.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/music-3090204__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/jazz-1658887__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/music-594275__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <br>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/nkululeko-mabena-436777.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/austin-neill-222825.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="center">
-                <img class="card-img-top img-fluid img-responsive" src="images/ben-neale-194644.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/simon-maennling-27234.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/singer-540771__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div class="text-right" style="padding: 3px;">
-        <p><a href="#" class="text-muted">See all...</a></p>
-    </div>
-    <br>
-    <h3 id="discover">Discover something amazing ...</h3>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/israel-palacio-459693.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/4902320203_29e3235af5_z.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/music-3090204__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/jazz-1658887__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/music-594275__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <br>
-    <div class="card-deck">
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/nkululeko-mabena-436777.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-        <br>
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/austin-neill-222825.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="center">
-                <img class="card-img-top img-fluid img-responsive" src="images/ben-neale-194644.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/simon-maennling-27234.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="text-center">
-                <img class="card-img-top img-fluid img-responsive" src="images/singer-540771__340.jpg" alt="Card image cap">
-                <div class="card-body text-left">
-                  <p class="card-text"><span class="title">Title: </span></p>
-                  <p class="card-text"><span class="artist">Artist: </span></p>
-                  <p class="card-text"></p>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div class="text-right" style="padding: 3px;">
-        <p><a href="#" class="text-muted">See all...</a></p>
-=======
+            
     <h3 id="newly_added">Newly added...</h3>
     <div class="row">
         <!--Displaying New songs -->
@@ -482,9 +119,8 @@
     </div>
     <div class="text-right" style="padding: 3px;">
         <p><a href="main.php?ID=1" class="">See all...</a></p>
->>>>>>> branch1
     </div>
-</div>
     
+</div>   
 </body>
 </html>
